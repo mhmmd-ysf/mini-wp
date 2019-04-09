3dacd4b865b71eeff1e2388374f2bcde86242c2f
mhmmd-ysf <muhammad_yusuf22@outlook.co.id>
2019-04-09 23:44:40 +0700

    Switched files from 'tes' (development) folder

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
+let url = 'http://localhost:3000'
+
+new Vue({
+  el: '#app',
+  data: {
+    lirik: 'lirik',
+    message: 'ey',
+    articles: [],
+    isShown: true,
+    title: '',
+    content: '',
+    search: '',
+    titleEdit: '',
+    contentEdit: ''
+  },
+  created() {
+    // console.log('masuk')
+    axios.get(url + "/articles")
+      .then(({ data }) => {
+        // console.log(data)
+        this.articles = data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  },
+  computed: {
+    validation() {
+      return this.title.length > 4 && this.title.length < 13
+    },
+    filteredList() {
+      let filtered = this.articles.filter(post => {
+        return post.title.toLowerCase().includes(this.search.toLowerCase()) ||post.content.toLowerCase().includes(this.search.toLowerCase())
+      })
+      if(filtered.length > 5) {filtered.length = 5}
+      return filtered
+    }
+  },
+  methods: {
+    test() {
+      let obj = {
+        title: this.title,
+        content: this.content,
+        modified: new Date()
+      }
+      axios.post(url + '/articles', obj)
+        .then(({data}) => {
+          let sorted = data.sort((a,b) => {
+            return b.modified - a.mmodified
+          })
+          this.articles = sorted
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  watch: {
+    searches: function (input) {
+
+    }
+  }
+})
+
+/*
+Toggle Hide/Show Jquery
 $(document).ready(function() {
   toggleAddArticle()
 })
@@ -16,3 +82,4 @@ function toggleAddArticle() {
     content.style.display = "block";
   }
 }
+*/
